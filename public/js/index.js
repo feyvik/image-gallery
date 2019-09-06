d1ff1608395c2ffdf95ed2c80fa378458e5717f9
Vivianemma <vivianemma97@gmail.com>
2019-09-06 16:33:52 +0100

    add loader

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,5 @@
-let start = 0, end = 10, buffer = [];
+let start = 0, end = 3, buffer = [];
 let key = "open";
-
 // nav
 window.addEventListener("scroll", function () {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -19,53 +18,51 @@ const getHeight = element => {
 let offset = `${getHeight(document.querySelector(".upper")) + getHeight(document.querySelector(".nav-up"))}`;
 window.addEventListener("scroll", function () {
   if (document.body.scrollTop > offset || document.documentElement.scrollTop > offset) {
-    document.querySelector('.input-container').style.display = 'block';
+    document.querySelector('.input-field').style.display = 'block';
   } else {
-    document.querySelector(".input-container").style.display = 'none';
+    document.querySelector(".input-field").style.display = 'none';
   }
 });
 
 // calling the api
 window.onload = () => {
-  fetch('https://picsum.photos/v2/list?limit=30')
-    .then(response => {
-      return response.json();
-    })
-    .then(arrayOfObjects => {
-      buffer.push(...arrayOfObjects);
-      displayImages(buffer, start, end);
-      //  loadImage()
-    })
-    .catch(err => {
-      document.getElementById('display').innerHTML += `
-          <div class="col-lg-3 col-md-3 col-sm-12 display border">
-            <img src="" alt="" data-lazy="" class="lazy-loading">
-            <p class="text-red author"></p>
-          </div>
-        `;
-      console.log(err);
-    });
+  document.querySelector('.loader').style.display = 'block'
+  // the formal url https://picsum.photos/v2/list?limit=30
+  fetch('https://api.unsplash.com/photos/?client_id=fdf429cca1201279179e94e631ceaf652780d35275fec51707aaeca1a23e0f0f')
+  .then(response => {
+    return response.json();
+  })
+  .then(arrayOfObjects => {
+    buffer.push(...arrayOfObjects);
+    displayImages(buffer, start, end);
+    document.querySelector('.loader').style.display = 'none'
+    console.log(arrayOfObjects)
+  })
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 // displayImages();
 function displayImages(buffer, start, end) {
-  if (buffer.length == 0) return;
+  if (buffer.length == 0){
+    return document.querySelector('.more').style.display = 'block';
+  } 
   let shortArray = buffer.splice(start, end);
   shortArray.map(obj => {
-    let { author, download_url, id } = obj;
+    let {urls, id, } = obj;
     document.getElementById('display').innerHTML += `
-          <div id="${id}" class="col-lg-3 col-md-3 col-sm-12 display">
-            <img src="${download_url}" alt="" data-lazy="" class="lazy-loading">
-            <p class="text-red author">${author}</p>
+          <div id="${id}" class="col-lg-3 col-md-4 col-sm-12 display">
+            <img src="${urls.thumb}" alt="" data-lazy="" class="lazy-loading">
           </div>
         `;
-      })
+        // <p class="text-red author">${alt_description}</p>
+  })
   // Sets an observer for each image
   lazyTargets = document.querySelectorAll('.lazy-loading');
   lazyTargets.forEach(lazyLoad);
 }
 
-// the lazy loading 
 function lazyLoad(target) {
   const obs = new IntersectionObserver((entries, observer) => {
     entries.forEach(entry => {
@@ -84,13 +81,8 @@ function lazyLoad(target) {
 // infinit scroller
 window.addEventListener("scroll", function () {
   if (Number(document.documentElement.scrollTop) + Number(document.documentElement.clientHeight) - Number(document.body.clientHeight) >= 0) {
-    document.querySelector('.loader').style.display = 'block';
-    if(key == "open"){
-      this.console.log(key)
       displayImages(buffer, start, end);
-      document.querySelector('.loader').style.display = 'none';
-    }else{
-      document.querySelector('.loader').style.display = 'none';
+      document.querySelector('.loader').style.display = 'none'
+    }else {
     }
-  }
 });
